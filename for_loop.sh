8790496a0c3bd22a19609fff2c4fe7aefd42289d
Ubuntu <ubuntu@ip-172-31-38-215.ec2.internal>
2026-08-30 22:20:02 +0530

    for, while and read practice

---
+++ for_loop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sleep 5
+for num in {1..10}; do
+	sleep 5
+	echo $num
+done
---
+++ read_input.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+
+#-p is to display custom message before input taking and -s is for to hide content while typing
+read -p "Enter a name:" -s name
+echo 
+echo "welcome $name"
+
